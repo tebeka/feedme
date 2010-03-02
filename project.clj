b0a70540b527ca02de8d34faf02d013128c4bfb0
Miki Tebeka <miki.tebeka@gmail.com>
2010-03-02 07:44:40 -0800

    rome-1.0 with :repositories

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
 
 (defproject feedme "1.0.0-SNAPSHOT"
   :description "Easy access to RSS/Atom Feeds"
+  :repositories [["java.net" "http://download.java.net/maven/2/"]]
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [jdom "1.1"]])
+                 [rome "1.0"]])
