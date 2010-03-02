e990329d2570201374e975b8adcebd228bde7b40
Miki Tebeka <miki.tebeka@gmail.com>
2010-03-02 07:45:05 -0800

    Remove comment

--- project.clj
+++ project.clj
@@ -1,7 +1,3 @@
-; FIXME: Rome latest version is 1.0, however the maven repo has on 0.9 (from
-; 2006). Currently we host rome-1.0.jar locally in lib and require jdom. Find a
-; way to get the right Rome jar (ask comp.lang.clojure)
-
 (defproject feedme "1.0.0-SNAPSHOT"
   :description "Easy access to RSS/Atom Feeds"
   :repositories [["java.net" "http://download.java.net/maven/2/"]]
