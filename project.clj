b10c066f82ee4cd414eb5ccf2f82f88e4fe1bbfc
Miki Tebeka <miki.tebeka@gmail.com>
2010-09-24 09:02:28 -0700

    Clojure 1.2 and new lein syntax for repositories

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject feedme "0.0.2"
   :description "Easy access to RSS/Atom Feeds"
   :url "http://bitbucket.org/tebeka/feedme/"
-  :repositories [["java.net" "http://download.java.net/maven/2/"]]
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]
+  :repositories {"java.net" "http://download.java.net/maven/2/"}
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [rome "1.0"]])
