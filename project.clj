90f628ff2eb5876ffb474ba644776841a5b19d77
Miki Tebeka <miki.tebeka@gmail.com>
2010-12-30 10:06:33 -0800

    Project url to /src so README is shown

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject feedme "0.0.3"
   :description "RSS/Atom feed parser based on ROME"
-  :url "http://bitbucket.org/tebeka/feedme/"
+  :url "http://bitbucket.org/tebeka/feedme/src"
   :repositories {"java.net" "http://download.java.net/maven/2/"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
